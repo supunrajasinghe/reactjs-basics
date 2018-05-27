01b6753e3b06f9b16340a48ac6670f1582dac9a9
supun <supunrajasinghe@gmail.com>
2018-05-28 00:27:21 +0530

    navigation

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,88 +1,13 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-export class Home extends React.Component {
-    constructor(props){
-        super();
-        this.state = {
-            age : props.initialAge,
-            status : 0,
-            homeLink: props.initialLinkName
-        };
-        setTimeout(()=>{
-            this.setState({
-                status:1
-            });
-        },3000)
-        console.log("Constructor");
-    }
 
-    onMakeOlder(){
-        this.setState({
-            age : this.state.age + 3,
-        });
-    }
 
-    componentWillMount(){
-        console.log("Component will mount");
-    }
-
-    componentDidMount(){
-        console.log("component did mount");
-    }
-
-    componentWillReceiveProps(nextProps){
-        console.log("Component Will recieve props ",nextProps);
-    }
-
-    shouldComponentUpdate(nextProps,nextState){
-        console.log("Should component update ",nextProps,nextState);
-        return true;
-    }
-
-    componentWillUpdate(nextProps,nextState){
-        console.log("Component will update",nextProps,nextState);
-    }
-
-    componentDidUpdate(prevProps,prevState){
-        console.log("component did update",prevProps,prevState);
-    }
-
-    componentWillUnmount(){
-        console.log("component will unmount");
-    }
-
-    onChangeLink(){
-        this.props.changeLink(this.state.homeLink);
-    }
-
-    onHandleChange(event){
-        this.setState({
-            homeLink:event.target.value,
-        });
-    }
-
+export class Home extends React.Component{
     render(){
         return(
             <div>
-                <p>In a new component!</p>
-                <p>Your name is {this.props.name}, Your age is {this.state.age}</p>
-                <p>Status : {this.state.status}</p>
-                <hr/>
-                <button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me older!</button>
-                <hr/>
-                <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
-                <hr/>
-                <input type="text" value={this.state.homeLink} onChange={(event) => this.onHandleChange(event)}/>
-                <button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">Change Header Link</button>
+                <h3>HOME</h3>
             </div>
         );
     }
 }
-
-Home.PropTypes = {
-    name: PropTypes.string,
-    initialAge: PropTypes.number,
-    greet: PropTypes.func,
-    initialLinkName:PropTypes.string
-};
--- src/app/index.js
+++ src/app/index.js
@@ -1,63 +1,29 @@
-import React from "react";
-import {render} from "react-dom";
+import React from  "react";
+import { render } from "react-dom";
+import { browserHistory} from "react-router";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import {Header} from "./components/Header";
-import {Home} from "./components/Home";
+import { Root } from "./components/Root";
+import { Home } from "./components/Home";
+import { User } from "./components/User";
 
 class App extends React.Component {
-    constructor(){
-        super();
-        this.state={
-            homeLink: "Home",
-            homeMounted:true
-        };
-    }
+    render() {
+        return (
+            <Router history={browserHistory}>
+                <div>
+                    <Route exact path="/" component={Root}/>
+                    <Route exact path="/" component={Home}/>
 
-    onGreet(){
-        alert("Hello!");
-    }
+                    <Route path="/user" component={Root}/>
+                    <Route path="/user" component={User}/>
 
-    onChangeLinkName(newName){
-        this.setState({
-           homeLink:newName
-        });
-    }
-
-    onChangeHomeMounted(){
-        this.setState({
-            homeMounted: !this.state.homeMounted
-        });
-    }
-
-    render(){
-        let homeCmp = "";
-        if (this.state.homeMounted){
-            homeCmp = (
-                <Home
-                    name={"max"}
-                    initialAge={27}
-                    greet={this.onGreet}
-                    changeLink={this.onChangeLinkName.bind(this)}
-                    initialLinkName={this.state.homeLink}
-                />
-            );
-        }
-        return(
-          <div className="container">
-              <div className="row">
-                  <div className="col-xs-10 col-xs-offset-1">
-                      <Header homeLink={this.state.homeLink}/>
-                  </div>
-                  <div className="col-xs-10 col-xs-offset-1">
-                      {homeCmp}
-                  </div>
-                  <div className="col-xs-10 col-xs-offset-1">
-                      <button onClick={this.onChangeHomeMounted.bind(this)} className="btn btn-primary">(un)mount home component</button>
-                  </div>
-
-              </div>
-          </div>
-        );
+                    <Route path="/home" component={Root}/>
+                    <Route path="/home" component={Home}/>
+                </div>
+            </Router>
+        )
     }
 }
+
 render(<App/>, window.document.getElementById("app"));
